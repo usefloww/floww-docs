302b0ff6fd9c865f280c8bd1961b3f96441a0103
toon10 <toon@techwolf.ai>
2025-11-16 19:11:43 +0100

    fix: build

--- docs/components/ArchitectureDiagram.tsx
+++ docs/components/ArchitectureDiagram.tsx
@@ -1,6 +1,6 @@
 import { useDark } from "rspress/runtime";
 
-export function ArchitectureDiagram() {
+export default function ArchitectureDiagram() {
     const dark = useDark();
     return <iframe src={`https://link.excalidraw.com/readonly/WPfZVHs9GgNZfEfejzN0?darkMode=${dark}`} width="100%" height="500px" />;
   
